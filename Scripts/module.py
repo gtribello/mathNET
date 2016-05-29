4c7e91a816e73fd03d95ddd3679770ffcc5787be
Gareth Tribello <gareth.tribello@gmail.com>
2016-05-29 14:44:02 +0100

    Added more information to module description

--- Scripts/module.py
+++ Scripts/module.py
@@ -30,20 +30,55 @@ class module(object):
       mtlist = mfile.readlines()
       mfile.close()
 # This does the generation of the graph
-      for j in range(1,len(mtlist)):
-         clista = mtlist[j-1].split()
-         clistb = mtlist[j].split()
-         if (len(clista)>0) & (len(clistb)>0) :
-            assert( (len(clista)==1) & (len(clistb)==1) )
-            assert( (clista[0] in nodelist) & (clistb[0] in nodelist) )
-            graph[ nodelist.index(clista[0]), nodelist.index(clistb[0]) ] += 1
-            self.mygraph[ nodelist.index(clista[0]), nodelist.index(clistb[0]) ]=1
+      ingraph = 0
+      clista = []
+      for item in mtlist :               # range(1,len(mtlist)):
+         if ingraph==1 :
+            clistb = item.split()
+            if (len(clista)>0) & (len(clistb)>0) :
+               assert( (len(clista)==1) & (len(clistb)==1) )
+               assert( (clista[0] in nodelist) & (clistb[0] in nodelist) )
+               graph[ nodelist.index(clista[0]), nodelist.index(clistb[0]) ] += 1
+               self.mygraph[ nodelist.index(clista[0]), nodelist.index(clistb[0]) ]=1
+            clista=clistb
+         elif "GRAPH:" in item  :
+            ingraph=1
+            
       return graph
 
    def printModulePage(self):
       f = open( 'Templates/module_template.html', 'r' )
       outfilecontents = f.read()
       f.close()
+      # Now open the file ane read in the lines
+      mfile = open( "Modules/" + self.name, 'r' )
+      mtlist = mfile.readlines()
+      mfile.close()
+      indesc = 0
+      desc = ""
+      inlearn = 0
+      learning = "<ul> \n"
+      for item in mtlist : 
+          if "TITLE:" in item :
+             outfilecontents = outfilecontents.replace("INSERT FULL NAME", item.replace("TITLE:","") )
+          elif "LEARNINGOUTCOMES:" in item :
+             if indesc!=1 :
+                sys.error("found LEARNINGOUTCOMES: before DESCRIPTION:")
+             indesc = 0
+             inlearn = 1
+          elif indesc==1 :
+             desc += item 
+          elif (inlearn==1) & (item[0]=="-") :
+             learning += '<li type="square">' + item[1:] + '</li> \n'
+          elif "DESCRIPTION:" in item :
+             indesc=1
+          elif "GRAPH:" in item  :
+             if inlearn!=1 :
+                sys.error("found GRAPH: before LEARNINGOUTCOMES:")
+             break
+      
+      outfilecontents = outfilecontents.replace("INSERT FULL DESCRIPTION", desc )
+      outfilecontents = outfilecontents.replace("INSERT LEARNING OUTCOMES", learning + "</ul>\n" )    
       outfilecontents = outfilecontents.replace("INSERT MODULE LABEL", self.name)
       outfilecontents = outfilecontents.replace("INSERT MODULE LIST", self.inlist.getModuleMenu() )
       outfile = open('html/' + self.name + '.html', 'w' )

299ed599f976e1faca7b90696f2396be35c6a619
Gareth Tribello <gt@eider.phy.qub.ac.uk>
2016-12-13 11:09:53 +0000

    Added stuff to do problems in xml as well

--- Scripts/xml_to_html.py
+++ Scripts/xml_to_html.py
@@ -40,14 +40,77 @@ def build_html_file( infile ):
        page = page.replace( "INSERT GEOGEBRA", tree.find("GEOGEBRA").text )
        page = page.replace( "INSERT DESCRIPTION", tree.find("DESCRIPTION").text )
        page = page.replace( "INSERT QUESTIONS", ET.tostring( tree.find("UL"), encoding="unicode", method="xml") )
+    else : 
+       html_string = "<H1> Introduction </H1>\n" 
+       html_string += ET.tostring( tree.find("DESCRIPTION"), encoding="unicode", method="xml" ) 
+       html_string += "<H1> Example problems </H1>\n"
+       html_string += "<p> Click on the problems to reveal the solution </p> \n" 
+       root = tree.getroot()
+       iq = 0
+       egcount=0
+       for child in root :
+           if child.tag=="QUESTION_GROUP" :
+              egcount += 1
+              html_string += '<h4> Problem ' + str(egcount) + '</h4>'
+              iq, newstr = questionsToHTML( iq, child )
+              html_string += newstr 
+           elif child.tag=="EXAMPLE" :
+              html_string += exampleToHTML( iq, child )
+              iq+=1
+           elif child.tag=="EXERCISE" :
+              html_string += '<H1> Problems for you to try </H1> \n'
+              html_string += '<div id="accordion" class="panel-group"> \n'
+              html_string += '    <div class="panel panel-default">\n'
+              html_string += '        <div class="panel-heading">\n'
+              html_string += '            <h4 class="panel-title">\n'
+              html_string += '               <a class="accordion-toggle" href="#collapse-' + str(iq) +'" data-parent="#accordion" data-toggle="collapse">\n'
+              html_string += '                  ' + child.find("TITLE").text + '\n'
+              html_string += '               </a>\n'
+              html_string += '            </h4>\n'
+              html_string += '        </div>\n'
+              html_string += '        <div id="collapse-' + str(iq) + '" class="accordion-body collapse in">\n'
+              html_string += '            <div class="panel-body">\n'
+              html_string += '               <div class="row">\n'
+              html_string += ET.tostring( child.find("PROBLEMS"), encoding="unicode", method="xml" )  
+              html_string +='         </div>\n'
+              html_string +='      </div>\n'
+              html_string +='   </div>\n'
+              html_string +='</div>\n'
+              iq+=1
+       page = page.replace("INSERT EXERICSE MATERIAL", html_string )
 
-    # content = ""
-    # for child in root :
-    #    if child.tag=="UL" :
-    #       content += ET.tostring( child, encoding="unicode", method="xml") 
-
-    # page = page.replace("INSERT QUESTIONS", content )
     # And output this to an html file
     f = open('html/' + infile + '.html', 'w' )
     f.write( page )
     f.close()
+
+def exampleToHTML( iq, child ):
+    html_string =  '<div id="accordion" class="panel-group"> \n'
+    html_string += '<div class="panel panel-default">\n'
+    html_string += '    <div class="panel-heading">\n'
+    html_string += '        <h4 class="panel-title">\n'
+    html_string += '        <a class="accordion-toggle" href="#collapse-' + str(iq) +'" data-parent="#accordion" data-toggle="collapse">\n'
+    html_string += ET.tostring( child.find("QUESTION"), encoding="unicode", method="xml" )
+    html_string += '        </a>\n'
+    html_string += '        </h4>\n'
+    html_string += '    </div>\n'
+    html_string += '    <div id="collapse-' + str(iq) + '" class="accordion-body collapse">\n'
+    html_string += '        <div class="panel-body">\n'
+    html_string += '           <div class="row">\n'
+    html_string += ET.tostring( child.find("SOLUTION"), encoding="unicode", method="xml" )
+    html_string +='         </div>\n'
+    html_string +='      </div>\n'
+    html_string +='   </div>\n'
+    html_string +='</div>\n'
+    html_string += '</br>\n'
+    return html_string 
+
+def questionsToHTML( iq, child ):
+    html_string = ""
+    for sub in child :
+        if sub.tag=="EXAMPLE" :
+           html_string += exampleToHTML( iq, sub )
+           iq += 1
+    return iq, html_string
+
+
